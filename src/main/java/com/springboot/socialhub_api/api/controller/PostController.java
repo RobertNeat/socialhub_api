470582aa96f88a4126f8f85b343aec8915891e03
Robert Sereda <robertgoogl@gmail.com>
2023-11-03 11:52:14 +0100

    added post image upload/download

--- src/main/java/com/springboot/socialhub_api/api/controller/PostController.java
+++ src/main/java/com/springboot/socialhub_api/api/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.springboot.socialhub_api.api.controller;
 
+import com.springboot.socialhub_api.api.config.FileUploadProperties;
 import com.springboot.socialhub_api.api.model.User;
 import com.springboot.socialhub_api.api.model.Comment;
 import com.springboot.socialhub_api.api.model.Post;
@@ -12,14 +13,22 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.swing.text.html.Option;
+import java.io.File;
+import java.nio.file.Files;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @CrossOrigin("http://127.0.0.1:5500")
 @RestController
@@ -29,6 +38,8 @@ public class PostController {
     private final UserRepository userRepository;
 
     private final AuthService authService;
+    @Autowired
+    private FileUploadProperties fileUploadProperties;
 
     public PostController(PostRepository postRepository, UserRepository userRepository,AuthService authService) {
         this.postRepository = postRepository;
@@ -73,6 +84,55 @@ public class PostController {
         return postRepository.save(newPost);
         }else{
             return null;
+        }
+    }
+
+    //upload image to post
+    @PostMapping(path="/image",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> uploadImage(@RequestHeader("Authorization")String token,@RequestParam("postId") int post_id ,@RequestParam("image") MultipartFile file) {
+        if (authService.isLoggedIn(token)) {
+            Optional<Post> post_query = postRepository.findById(post_id);
+            if (post_query.isPresent()) {
+                Post post = post_query.get();
+                String filePath = fileUploadProperties.getPath();
+
+                String originalFilename = file.getOriginalFilename();
+                String fileExtension = StringUtils.getFilenameExtension(originalFilename);
+                String randomFileName = UUID.randomUUID().toString() + "." + fileExtension;
+
+                try {
+                    file.transferTo(new File(filePath + randomFileName));
+                    System.out.println(filePath + randomFileName);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+                post.setImage(randomFileName);
+                Post saved_post = postRepository.save(post);
+                return ResponseEntity.status(HttpStatus.OK).body(saved_post);
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Post not found in DB");
+        }
+    }return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Not authorized");
+    }
+
+    //get the post image
+    @GetMapping(path="/image/{image}",produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
+    public ResponseEntity<?> downloadImage(@PathVariable("image") String image_name){
+        Optional<Post> post_query = postRepository.findByImageName(image_name);
+        String location = fileUploadProperties.getPath();
+        if(post_query.isPresent()){
+            String file_path = location+post_query.get().getImage();
+            try{
+                byte[] image = Files.readAllBytes(new File(file_path).toPath());
+                //return image;
+                return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/jpg")).body(image);
+            }catch(Exception e) {
+                System.out.println(e.getMessage());
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Image reading error");
+            }
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error fetching image resource");
         }
     }
 
--- src/main/java/com/springboot/socialhub_api/api/repositories/PostRepository.java
+++ src/main/java/com/springboot/socialhub_api/api/repositories/PostRepository.java
@@ -3,9 +3,15 @@ package com.springboot.socialhub_api.api.repositories;
 import com.springboot.socialhub_api.api.model.Post;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.springboot.socialhub_api.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post,Integer> {
     List<Post> findAllByUserId(int userId);
+
+    @Query("SELECT p FROM Post p WHERE p.image = :image")
+    Optional<Post> findByImageName(String image);
 }
