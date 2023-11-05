6ea1b6d61802db44e9fbcf08989dcb2e111a2712
Robert Sereda <robertgoogl@gmail.com>
2023-11-05 15:06:39 +0100

    added tests for comment and post (controllers mogification)

--- src/main/java/com/springboot/socialhub_api/api/controller/CommentController.java
+++ src/main/java/com/springboot/socialhub_api/api/controller/CommentController.java
@@ -36,81 +36,92 @@ public class CommentController {
 
     //get the comment
     @GetMapping("/{comment_id}")
-    public Optional<Comment> select_post(@RequestHeader("Authorization")String token,@PathVariable("comment_id") int id)
-    {
-        if(authService.isLoggedIn(token)){
-            return commentRepository.findById(id);
-        }else{
-            return null;
+    public ResponseEntity<Comment> selectComment(@RequestHeader("Authorization") String token, @PathVariable("comment_id") int id) {
+        if (authService.isLoggedIn(token)) {
+            Optional<Comment> comment = commentRepository.findById(id);
+            if (comment.isPresent()) {
+                return ResponseEntity.ok(comment.get());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
         }
     }
 
     //get comments by post
     @GetMapping("/all/{post_id}")
-    public List<Comment> all_comments(@RequestHeader("Authorization")String token,@PathVariable("post_id") int id)
-    {
-        if(authService.isLoggedIn(token)){
-            return commentRepository.findAllByPostId(id);
-        }else{
-            return null;
+    public ResponseEntity<List<Comment>> allComments(@RequestHeader("Authorization") String token, @PathVariable("post_id") int id) {
+        if (authService.isLoggedIn(token)) {
+            List<Comment> comments = commentRepository.findAllByPostId(id);
+            return ResponseEntity.ok(comments);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
         }
     }
 
     //create the comment
     @PostMapping("create_comment/{post_id}/{user_id}")
-    public Comment createComment(
-            @RequestHeader("Authorization")String token,
+    public ResponseEntity<Comment> createComment(
+            @RequestHeader("Authorization") String token,
             @PathVariable("post_id") int postId,
             @PathVariable("user_id") int userId,
             @RequestBody Comment newComment
     ) {
-        if(authService.isLoggedIn(token)){
+        if (authService.isLoggedIn(token)) {
             // Fetch the associated post and user from the database
             Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Post not found with ID: " + postId));
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Post not found with ID: " + postId));
 
             User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with ID: " + userId));
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with ID: " + userId));
 
             // Set the fetched post and user for the new comment
             newComment.setPost(post);
             newComment.setUser(user);
 
             // Save the new comment
-            return commentRepository.save(newComment);
-        }else{
-            return null;
+            Comment savedComment = commentRepository.save(newComment);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedComment);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
         }
     }
 
     //update the comment
     @PutMapping
-    public ResponseEntity<Comment> update(@RequestHeader("Authorization")String token,@RequestBody Comment updateComment)
-    {
-        if(authService.isLoggedIn(token)){
+    public ResponseEntity<?> update(@RequestHeader("Authorization") String token, @RequestBody Comment updateComment) {
+        if (authService.isLoggedIn(token)) {
             Optional<Comment> commentFromDatabase = commentRepository.findById(updateComment.getId());
-            if(commentFromDatabase.isPresent()){
+            if (commentFromDatabase.isPresent()) {
                 String description = updateComment.getDescription();
                 Date creation_date = updateComment.getCreation_date();
 
-                if(description!=null){commentFromDatabase.get().setDescription(description);}
-                if(creation_date!=null){commentFromDatabase.get().setCreation_date(creation_date);}
+                if (description != null) {
+                    commentFromDatabase.get().setDescription(description);
+                }
+                if (creation_date != null) {
+                    commentFromDatabase.get().setCreation_date(creation_date);
+                }
 
                 return ResponseEntity.ok(commentRepository.save(commentFromDatabase.get()));
-            }else{
+            } else {
                 return ResponseEntity.notFound().build();
             }
-        }else{
-            return null;
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied");
         }
     }
 
     //delete the comment
     @DeleteMapping("/{comment_id}")
-    public void delete(@RequestHeader("Authorization")String token,@PathVariable("comment_id") int id) {
-        if(authService.isLoggedIn(token))
+    public ResponseEntity<Void> delete(@RequestHeader("Authorization") String token, @PathVariable("comment_id") int id) {
+        if (authService.isLoggedIn(token)) {
             commentRepository.deleteById(id);
+            return ResponseEntity.noContent().build(); // 204 No Content for successful deletion
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build(); // 403 Forbidden for unauthorized access
+        }
     }
-
-    
 }
+
